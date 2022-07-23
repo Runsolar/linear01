1c4e1a7bba3b3ce3d3ea7a9e11192e204edd8cbd
KatyaGorina <67708051+KatyaGorina@users.noreply.github.com>
2022-07-23 22:26:49 +0300

    added writing to the file and plor_gr function

--- visualization.py
+++ visualization.py
@@ -16,7 +16,18 @@ def plot_boundary_solutions(X, y, classifier, resolution=0.2, x_label='', y_labe
                                    np.arange(x2_min, x2_max, resolution))
 
     y_pred = np.array(classifier.predict(np.array([x1_mesh.ravel(), x2_mesh.ravel()]).transpose().tolist())).reshape(x1_mesh.shape)
-
+    with open("perc.mdl", "w") as file:
+        for i in range(len(y_pred)):
+            for j in range(len(y_pred[0])):
+                file.write(str(y_pred[i][j]) + " ")
+    with open("X1.mdl", "w") as file:
+        for i in range(len(x1_mesh)):
+            for j in range(len(x1_mesh[0])):
+                file.write(str(x1_mesh[i][j]) + " ")
+    with open("X2.mdl", "w") as file:
+        for i in range(len(x2_mesh)):
+            for j in range(len(x2_mesh[0])):
+                file.write(str(x2_mesh[i][j]) + " ")
     plt.contourf(x1_mesh, x2_mesh, y_pred, cmap=cmap, alpha=0.3)
     plt.xlim(x1_mesh.min(), x1_mesh.max())
     plt.ylim(x2_mesh.min(), x2_mesh.max())
@@ -31,7 +42,7 @@ def plot_boundary_solutions(X, y, classifier, resolution=0.2, x_label='', y_labe
     plt.title(title)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
-    plt.show()
+   # plt.show()
 
 
 def plot_graph(x, y, x_label, y_label, tittle):
@@ -40,4 +51,23 @@ def plot_graph(x, y, x_label, y_label, tittle):
     plt.title(tittle)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
+    plt.show()
+
+
+def plor_gr(X, y, resolution=0.2):
+    #markers = ('s', 'x', 'o')
+    colors = ('red', 'blue', 'lightgreen', 'gray')
+
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+    X[0], X[1] = X[0][:-1], X[1][:-1]
+    X[0] = [float(x) for x in X[0]]
+    X[1] = [float(x) for x in X[1]]
+    x1_min, x1_max = min(X[0]) - 2, max(X[0]) + 2
+    x2_min, x2_max = min(X[1]) - 2, max(X[1]) + 2
+    x1_mesh, x2_mesh = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                                   np.arange(x2_min, x2_max, resolution))
+    plt.contourf(X[0], X[1], np.array(y).reshape(x1_mesh.shape), cmap=cmap, alpha=0.3)
+    plt.xlim(X[0].min(), X[0].max())
+    plt.ylim(X[1].min(), X[1].max())
+    plt.legend()
     plt.show()
